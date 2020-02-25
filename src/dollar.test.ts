0d5f5df231eea58d54fc35d3f1943ddb79cc56fd
hisomura <hisomura.noreply@gmail.com>
2020-02-26 08:21:09 +0900

    p.22 equals() の実装

--- src/dollar.test.ts
+++ src/dollar.test.ts
@@ -6,4 +6,8 @@ describe('Dollarクラス', () => {
     expect(five.times(3).amount).toBe(15)
     expect(five.times(2).amount).toBe(10)
   })
+  test('等価', () => {
+    expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy()
+    expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy()
+  })
 })
--- src/dollar.ts
+++ src/dollar.ts
@@ -3,6 +3,9 @@ class Dollar {
   times(multiplier: number) {
     return new Dollar(this.amount * multiplier)
   }
+  equals(dollar: Dollar): boolean {
+    return this.amount === dollar.amount
+  }
 }
 
 export default Dollar
