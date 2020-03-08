bea2e92fffb2f45b2bf515f9a97710d1772e8474
hisomura <hisomura.noreply@gmail.com>
2020-03-08 17:27:48 +0900

    書籍に合わせてxunit.tsに全クラスをまとめた

--- chapter2/xunit.ts
+++ chapter2/xunit.ts
@@ -1,5 +1,52 @@
-import WasRun from './wasRun'
+import { strict as assert } from 'assert'
 
+class TestCase {
+  constructor(protected name: string) {}
+
+  hasMember(name: string): name is keyof TestCase {
+    return this.name in this
+  }
+
+  run() {
+    if (!this.hasMember(this.name)) {
+      throw new Error(`${this.constructor.name}に${this.name}は存在しない`)
+    }
+    // とりあえず型の警告が出ないようにした
+    // methodの型はhasMember()かrun()のものになっててこれで良いのかという気がする
+    const method = this[this.name]
+    if (!isCallable(method)) {
+      throw new Error(`${this.name}はcallableではない`)
+    }
+    method.call(this)
+  }
+}
+
+class WasRun extends TestCase {
+  public wasRun = false
+
+  constructor(protected name: keyof WasRun) {
+    super(name)
+  }
+
+  testMethod() {
+    this.wasRun = true
+  }
+}
+
+class TestCaseTest extends TestCase {
+  testRunning() {
+    const test = new WasRun('testMethod')
+    assert(!test.wasRun, 'error')
+    test.run()
+    assert(test.wasRun)
+  }
+}
+
+function isCallable(x: any): x is (y: any) => void {
+  return typeof x === 'function'
+}
+
+new TestCaseTest('testRunning').run()
 const test = new WasRun('testMethod')
 console.log(test.wasRun)
 test.run()
